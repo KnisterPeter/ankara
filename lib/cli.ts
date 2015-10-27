a3a1ff45111757fa90b12e4bec1c685e64457ab3
Markus Wolf <mail@markus-wolf.de>
2015-10-27 09:17:33 +0100

    Implemented 'all in one' cover command

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,29 +1,54 @@
 /// <reference path="../typings/references.d.ts" />
+import {prototype as modulePrototype} from 'module'; 
 import minimist from 'minimist';
 import * as globby from 'globby';
 import {sync as mkdirp} from 'mkdirp';
-import {sep, join, relative, dirname} from 'path';
-import {readFileSync, writeFileSync} from 'fs';
-import {instrumentFiles, generateLcov} from './index';
+import {sep, join, relative, dirname, resolve} from 'path';
+import {readFileSync, writeFileSync, existsSync} from 'fs';
+import {instrumentFile, instrumentFiles, generateLcov} from './index';
 import rc from 'rc';
 
+const cwd = process.cwd();
+const getExcludes = (config) => globby.sync(<string[]>config.excludes);
+const isCovered = (filename, excludes) => excludes.indexOf(relative(cwd, filename)) == -1;
+
 export default function(argv: string[]) {
-  const cwd = process.cwd();
   const config = rc('ankara', {
     extensions: ['.js'],
     excludes: ['**/node_modules/**']
   });
+  const excludes = getExcludes(config);
 
   const args = minimist(argv);
   switch (args._[0]) {
+    case 'cover':
+      const originalRequire = modulePrototype.require;
+      modulePrototype.require = function(id: string): string {
+        var moduleToLoad = id;
+        if (id[0] == '.') {
+          var resolvedPath = resolve(dirname(this.id), id);
+          if (!existsSync(resolvedPath)) {
+            var temp = resolvedPath;
+            resolvedPath = resolvedPath + config.extensions[0];
+            if (!existsSync(resolvedPath)) {
+              resolvedPath = temp + '.json';
+            }
+          }
+          if (isCovered(resolvedPath, excludes)) {
+            moduleToLoad = instrumentFile(resolvedPath);
+          }
+        }
+        return originalRequire.call(this, moduleToLoad);
+      }
+      originalRequire(join(cwd, args._[1]));
+      process.on('beforeExit', () => generateLcov());
+      break;
     case 'instrument':
-      const excludes = globby.sync(<string[]>config.excludes);
-      const isCovered = (filename) => excludes.indexOf(relative(cwd, filename)) == -1;
       globby.sync(<string[]>config.files).forEach(filename => {
         let next: string[] = [filename];
         do {
           const file = next.pop();
-          const covered = isCovered(file);
+          const covered = isCovered(file, excludes);
           next = next.concat(instrumentFiles(file, covered));
           if (covered) {
             console.log(`Instrumented ${relative(cwd, file)}`);
--- tests/run.js
+++ tests/run.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require('babel/register');
-require('../register');
+//require('../register');
 
 var glob = require('glob');
 var path = require('path');
